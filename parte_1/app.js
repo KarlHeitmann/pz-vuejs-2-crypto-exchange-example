a91f332bd5bc375c7e640b97372503bf38fb9b80
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2021-11-17 22:48:42 -0300

    agregado iteracion con directiva v-for

--- parte_1/app.js
+++ parte_1/app.js
@@ -6,7 +6,16 @@ new Vue({
             name: 'Bitcoin',
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
             changePercent: 0,
-            prices: [55000, 60000, 63000, 67000, 64000, 58000, 59000, 62000, 61000]
+            prices: [55000, 60000, 63000, 67000, 64000, 58000, 59000],
+            pricesWithDays: [
+                { day: 'Lunes', value: 55000 },
+                { day: 'Martes', value: 60000 },
+                { day: 'Miércoles', value: 63000 },
+                { day: 'Jueves', value: 67000 },
+                { day: 'Viernes', value: 64000 },
+                { day: 'Sábado', value: 58000 },
+                { day: 'Domingo', value: 59000 },
+            ]
         }
     }
 })
