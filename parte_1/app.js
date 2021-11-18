9c505302cabc33b294f450051ff5cc1fa559e83b
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2021-11-17 22:37:30 -0300

    esta con v-if, v-show

---
+++ parte_1/app.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+
+    data () {
+        return {
+            name: 'Bitcoin',
+            img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+            changePercent: 0,
+            prices: [55000, 60000, 63000, 67000, 64000, 58000, 59000, 62000, 61000]
+        }
+    }
+})
