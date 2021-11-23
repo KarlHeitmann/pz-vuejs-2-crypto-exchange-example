a50103ab807bf5ef460c51a1389326e38e5682a9
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2021-11-23 12:08:49 -0300

    agregado los filtros (clase 31)

--- platzi-exchange/src/main.js
+++ platzi-exchange/src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import '@/assets/css/tailwind.css' //@ es algo que entrega webpack y vue para hacer referencia de manera relativa al directorio source :D
 
 import router from '@/router'
+import { dollarFilter, percentFilter } from '@/filters'
 
+Vue.filter('percent', percentFilter)
+Vue.filter('dollar', dollarFilter)
 Vue.config.productionTip = false
 
 new Vue({
