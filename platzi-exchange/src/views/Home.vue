c178be76b9ca2ef2e6c4675e974d6a1a06129f29
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2021-11-23 11:49:49 -0300

    llama a la API, pero no funciona que se muestre la imagen

--- platzi-exchange/src/views/Home.vue
+++ platzi-exchange/src/views/Home.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
-    <px-assets-table />
+    <px-assets-table
+      :assets="assets"
+    /><!-- XXX HACK : escribir :assets es lo mismo que v-bind:assets -->
   </div>
 </template>
 
 <script>
+import api from '@/api'
 import PxAssetsTable from '@/components/PxAssetsTable'
 
 export default {
   name: 'Home',
   components: { PxAssetsTable },
+
+  data() {
+    return {
+      assets: [],
+    }
+  },
+
+  created() {
+    api.getAssets().then((assets) => (this.assets = assets))
+  },
 }
 </script>
