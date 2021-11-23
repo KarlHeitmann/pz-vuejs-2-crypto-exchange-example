6cbbb4ac137b2a09a8e14550b1f6315bd4bcaaa9
Karl Heitmann <karlheitmann@Karls-MacBook-Pro.local>
2021-11-23 10:27:48 -0300

    agregado tailwind al proyecto

---
+++ apuntres.md
@@ -0,0 +1,2 @@
+¿Como instalar tailwindcss en un proyecto vue de manera facil?
+> vue add @ianaya89/tailwind
--- platzi-exchange/src/main.js
+++ platzi-exchange/src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import "@/assets/css/tailwind.css" //@ es algo que entrega webpack y vue para hacer referencia de manera relativa al directorio source :D
 
 Vue.config.productionTip = false;
 
